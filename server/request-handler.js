2de7313232e0fe1dfbf0aacde8da92c8986084eb
katie-truong <khntruong@ucdavis.edu>
2018-07-03 16:45:31 -0700

    Connect server to client

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,7 +1,7 @@
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': '*',
+  'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
 
@@ -16,6 +16,8 @@ var example = {
 };
 
 var body = [];
+var counter = 0;
+
 
 var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
@@ -50,6 +52,8 @@ var requestHandler = function(request, response) {
       requestBody += data;
     });
     request.on('end', function() {
+      counter++;
+      requestBody.objectId = counter;
       body.push(JSON.parse(requestBody));
       response.end();
     });
